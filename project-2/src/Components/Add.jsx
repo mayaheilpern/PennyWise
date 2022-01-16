40c977bd2f4976db701902845eb5a804dcf156ac
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-01-16 16:55:08 -0500

    created base add form modal

---
+++ project-2/src/Components/Add.jsx
@@ -0,0 +1,53 @@
+export default function Add({
+  show,
+  setShow,
+  name,
+  setName,
+  amount,
+  setAmount,
+  date,
+  setDate,
+  handleSubmit,
+}) {
+  return (show) ? (
+    <div className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center">
+      <div className="bg-white rounded-md">
+        <header className="flex justify-end p-2" onClick={() => setShow(false)}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </header>
+        <hr />
+        <form className="grid grid-col-4 p-8" onSubmit={handleSubmit}>
+          <input
+            required
+            id="name"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+            className="rounded-md p-3 space-y-1"
+          />
+          <input
+            required
+            id="amount"
+            type="number"
+            value={amount}
+            placeholder="amount"
+            onChange={(e) => setAmount(e.target.valueAsNumber)}
+            className="rounded-md p-3 space-y-1"
+          />
+          <input
+            required
+            id="date"
+            type="text"
+            value={date}
+            placeholder="Date"
+            onChange={(e) => setDate(e.target.value)}
+            className="rounded-md p-3 space-y-1"
+          />
+          <button className="rounded-md p-3 bg-blue-500 space-y-1">Add</button>
+        </form>
+      </div>
+    </div>
+  ) : "";
+}
--- project-2/src/Components/Bill.jsx
+++ project-2/src/Components/Bill.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react';
 import api from "../services/bill";
+import Add from './Add';
 import Tables from "./Tables";
 
 export default function Bill() {
 
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState();
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     const request = async () => {
@@ -17,11 +22,33 @@ export default function Bill() {
     request();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fields = {fields: { name, amount, date }}
+    await api.post("/", fields);
+    setShow(false)
+    setName("");
+    setAmount("");
+    setDate();
+  }
+
   let total = data.reduce((a, v) => a = a + v.fields.amount, 0);
 
   return (
     <div>
       <h1>Bills</h1>
+      <button onClick={() => setShow(true)}>Add New Bill</button>
+      <Add trigger={show}
+        name={name}
+        setName={setName}
+        amount={amount}
+        setAmount={setAmount}
+        date={date}
+        setDate={setDate}
+        handleSubmit={handleSubmit}
+        show={show}
+        setShow={setShow}
+      />
       {loading ? (
         <span>Loading...</span>
         ) : (
