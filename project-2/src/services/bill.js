be9c954e392e07d37abb84c6544c1b7514a8b1d9
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-01-16 13:40:39 -0500

    created axos api

---
+++ project-2/src/services/bill.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASE = proccess.env.REACT_APP_API_BASE;
+const KEY = proccess.env.REACT_APP_API_KEY;
+
+const baseURL = `https://api.airtable.com/v0/${BASE}/bill`
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${KEY}`,
+  }
+});
+
+export default api;
