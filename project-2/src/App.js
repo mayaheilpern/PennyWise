c23d21bbb6396a68a29b5812165fb10b72e0ee0c
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-01-16 13:49:18 -0500

    created routes

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import DropdownNav from "./Components/DropdownNav";
+import Home from "./Components/Home";
+import Bill from "./Components/Bill";
+import Expense from "./Components/Expense";
+import Other from "./Components/Other";
+
 function App() {
   return (
     <>
-      <h1 className="text-center">Hello World!</h1>
+      <Navbar />
+      <DropdownNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bill" element={<Bill />} />
+        <Route path="/expense" element={<Expense />} />
+        <Route path="/other" element={<Other />} />
+      </Routes>
     </>
   );
 }
---
+++ project-2/src/Components/DropdownNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DropdownNav() {
+  return (
+    <div>
+      Dropdown Navbar
+    </div>
+  )
+}
